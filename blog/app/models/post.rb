04b4fb740904e3506daf54dcd3c1800284644a00
anilbhu <you@example.com>
2014-03-04 11:54:55 +0530

    first commit

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
  attr_accessible :title, :content
+ has_many :comments
  
  validates :title, :content, :presence => true
  #validates :title, :content, :lenght => {:minimum => 2}
